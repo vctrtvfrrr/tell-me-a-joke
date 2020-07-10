9c4b5e6180bc95f75128a058837628c5e1d0971e
Victor Otávio Ferreira <victor@otavioferreira.com.br>
2020-07-10 19:57:32 -0300

    improve e2e tests

--- tests/e2e/specs/complete.js
+++ tests/e2e/specs/complete.js
@@ -1,4 +1,6 @@
 describe("Complete interaction", () => {
+  let joke = "";
+
   it("starts with a screen that is neither happy nor sad", () => {
     cy.visit("/");
     cy.get(".overall")
@@ -12,7 +14,15 @@ describe("Complete interaction", () => {
     cy.url().should("include", "/sad");
   });
 
-  it("should change the route and open a modal", () => {
+  it("should change the route and generate a random joke in a modal", () => {
     cy.get(".modal-container").should("have.css", "display", "none");
+    cy.server();
+    cy.route("https://geek-jokes.sameerkumar.website/*").as("geekJokes");
+    cy.get(".overall").click({ force: true });
+    cy.wait("@geekJokes").then(({ response }) => {
+      joke = response.body.joke;
+      cy.get(".modal-container").should("not.have.css", "display", "none");
+      cy.get(".blockquote").contains(joke);
+    });
   });
 });
