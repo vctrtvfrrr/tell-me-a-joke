656bebea2e370b135d3c0e9d10aa06036657aea9
Victor Otávio Ferreira <victor@otavioferreira.com.br>
2020-07-10 19:57:17 -0300

    search and display of the joke within the modal

--- src/components/Modal.spec.js
+++ src/components/Modal.spec.js
@@ -5,9 +5,20 @@ import Modal from "@/components/Modal.vue";
 describe("Modal.vue", () => {
   it("must contain a LoadingSpinner component", () => {
     const wrapper = shallowMount(Modal);
+    expect(wrapper.vm.isLoading).toBe(true);
     expect(wrapper.findComponent({ name: "LoadingSpinner" }).exists()).toBe(
       true
     );
+  });
+
+  it("must contain a Quote component", () => {
+    const wrapper = shallowMount(Modal, {
+      data() {
+        return { isLoading: false };
+      }
+    });
+    expect(wrapper.vm.isLoading).toBe(false);
+    expect(wrapper.findComponent({ name: "Quote" }).exists()).toBe(true);
   });
 
   it("must come hidden by default", () => {
---
+++ src/components/Quote.vue
@@ -0,0 +1,51 @@
+<template>
+  <blockquote class="blockquote">{{ text }}</blockquote>
+</template>
+
+<script>
+export default {
+  name: "Quote",
+
+  props: {
+    text: {
+      type: String,
+      required: true
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+@import url("https://fonts.googleapis.com/css?family=Special+Elite");
+
+.blockquote {
+  font-family: "Special Elite", cursive;
+  font-weight: 100;
+  font-size: 1.2rem;
+  line-height: 1.4;
+  position: relative;
+  margin: 0;
+  padding: 0.5rem;
+
+  &:before,
+  &:after {
+    position: absolute;
+    color: #f1efe6;
+    font-size: 6rem;
+    width: 3rem;
+    height: 3.4rem;
+  }
+
+  &:before {
+    content: "“";
+    left: -2.8rem;
+    top: -1.5rem;
+  }
+
+  &:after {
+    content: "”";
+    right: -2.8rem;
+    bottom: -0.8rem;
+  }
+}
+</style>
