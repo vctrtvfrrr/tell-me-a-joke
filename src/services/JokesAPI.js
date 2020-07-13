c7617d586b2997216eed5ce549290db429dfbc84
Victor Otávio Ferreira <victor@otavioferreira.com.br>
2020-07-13 01:11:08 -0300

    in code documentation

--- src/services/JokesAPI.js
+++ src/services/JokesAPI.js
@@ -1,11 +1,16 @@
 import axios from "axios";
 
+// Creates the API instance with the custom settings
 const API = axios.create({
   baseURL: "https://geek-jokes.sameerkumar.website",
   headers: { Accept: "application/json" },
   timeout: 5000 // miliseconds
 });
 
+/**
+ * Get a joke in the "Geek Joke API".
+ * @return {Object}
+ */
 const getJoke = async () => {
   const response = {};
 
@@ -14,6 +19,8 @@ const getJoke = async () => {
     response.success = true;
     response.text = res.joke;
   } catch (error) {
+    // As the API has the habit of being often down,
+    // we return a user-friendly response.
     response.success = false;
     response.text = "I'm sorry, I think I lost my book of jokes...  🙄";
   }
